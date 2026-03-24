2f77a2d3c64ee441a1ead28f5d06213528fce916
Ricardo <landinezrichard@gmail.com>
2026-03-24 18:07:21 -0500

    feat: migración proyecto a TypeScript

---
+++ src/hooks/useCart.js
@@ -0,0 +1,89 @@
+import { useState, useEffect, useMemo } from "react";
+import { db } from "../data/db.js";
+
+export const useCart = () => {
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem("cart");
+    return localStorageCart ? JSON.parse(localStorageCart) : [];
+  };
+
+  const [data] = useState(db);
+  const [cart, setCart] = useState(initialCart);
+
+  const MAX_QUANTITY = 5;
+  const MIN_QUANTITY = 1;
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  function addToCart(item) {
+    const itemExist = cart.findIndex((itemInCart) => itemInCart.id === item.id);
+    // Si el item ya existe en el carrito, actualizamos la cantidad
+    if (itemExist >= 0) {
+      // Verificar si ya alcanzó el límite
+      if (cart[itemExist].quantity >= MAX_QUANTITY) return;
+
+      const updatedCart = [...cart];
+      updatedCart[itemExist].quantity++;
+      // Incrementar cantidad
+      setCart(updatedCart);
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    }
+  }
+
+  function removeFromCart(id) {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== id));
+  }
+
+  function increaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity < MAX_QUANTITY) {
+        return {
+          ...item,
+          quantity: item.quantity + 1,
+        };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  }
+
+  function decreaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity > MIN_QUANTITY) {
+        return {
+          ...item,
+          quantity: item.quantity - 1,
+        };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  }
+
+  function clearCart() {
+    setCart([]);
+  }
+
+  // State derivado
+  const isEmpty = useMemo(() => cart.length === 0, [cart]);
+  const cartTotal = useMemo(
+    () => cart.reduce((total, item) => total + item.price * item.quantity, 0),
+    [cart],
+  );
+
+  return {
+    data,
+    cart,
+    addToCart,
+    removeFromCart,
+    increaseQuantity,
+    decreaseQuantity,
+    clearCart,
+	isEmpty,
+	cartTotal,
+  };
+};
