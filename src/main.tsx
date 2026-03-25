140034db7aadf90fa32c2a1e197699228042b91e
Ricardo <landinezrichard@gmail.com>
2026-03-24 20:00:47 -0500

    fix: usando el non-null assertion operator

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.js'
 
-createRoot(document.getElementById('root')).render(
+// ! --> es necesario el ! para indicarle a TS que el elemento no es null, ya que getElementById puede retornar null
+// ! --> es un non-null assertion operator, que le dice a TS que confíe en nosotros y que el valor no será null o undefined en tiempo de ejecución. Es útil cuando sabemos que el elemento existe en el DOM pero TS no puede inferirlo.
+
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
